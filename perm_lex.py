46158cfd96dcc238f79b4b130afad7827c41102c
Aria Pegah <apegah@calpoly.edu>
2021-01-13 12:06:55 -0800

    started project

--- perm_lex.py
+++ perm_lex.py
@@ -1,3 +1,11 @@
-def perm_gen_lex(a): 
+def perm_gen_lex(a):
+    if len(a) == 0:
+        string_list = []
+    elif len(a) == 1:
+        string_list = [a]
+    else:
+        for char in a:
+            new_a = a[1:]
+            perm_gen_lex(new_a)
 
  
