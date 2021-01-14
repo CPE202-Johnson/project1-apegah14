f2504fbd8114395e7c847aef55d31fe89e9ff388
Aria Pegah <apegah@calpoly.edu>
2021-01-14 14:45:39 -0800

    changed list to string and bug fixes

--- base_convert.py
+++ base_convert.py
@@ -1,11 +1,14 @@
+# int int -> int
+# converts a base 10 number into a base b number
 def convert(num, b):
-    if num // b == 0:
-        converted_num = []
+    if num // b == 0:   # base case
+        if num > 9:     # convert to A-F in cases of values greater than 9
+            num = chr(num + 55)
+        converted_num = str(num)
     else:
-        remainder = num % b
-        quotient = num // b
-        converted_num = convert(quotient, b) + [remainder]
+        remainder = num % b     # int value for remainder
+        if remainder > 9:       # convert to A-F in cases of values greater than 9
+            remainder = chr(remainder + 55)
+        quotient = num // b     # int for quotient value
+        converted_num = str(convert(quotient, b)) + str(remainder)      # construct the converted value as a string
     return converted_num
-
-if __name__ == "__main__":
-    print(convert(13, 2))
