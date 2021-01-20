694d20e0a38c6f2b9371d88806bb587db9f298cc
Aria Pegah <apegah@calpoly.edu>
2021-01-19 17:27:00 -0800

    added additional comments

--- bears.py
+++ bears.py
@@ -1,24 +1,24 @@
 # int -> boolean
 # determine whether or not the game can be won with a specified number of bears
 def bears(n):
-    if n == 42:
+    if n == 42:     # success case
         return True
-    elif n < 42:
+    elif n < 42:    # failure case
         return False
     else:
         nums = [0, 0, 0]
         if n % 2 == 0:  # n is even
             nums[0] = n // 2
-        if n % 3 == 0 or n % 4 == 0:  # n is divisible by 3 or 4
-            last_two = n % 100  # get last 2 digits of n
-            ones = last_two % 10
-            tens = last_two // 10
+        if n % 3 == 0 or n % 4 == 0:    # n is divisible by 3 or 4
+            last_two = n % 100          # get last 2 digits of n
+            ones = last_two % 10        # separate ones place
+            tens = last_two // 10       # separate tens place
             if ones == 0 or tens == 0:
                 nums[1] = 0
             else:
                 nums[1] = n - (ones * tens)
         if n % 5 == 0:  # n is divisible by 5
             nums[2] = n - 42
-        for num in nums:
+        for num in nums:    # iterate through all rules
             if bears(num) is True:
                 return True
--- perm_lex.py
+++ perm_lex.py
@@ -1,22 +1,21 @@
 # string list -> string list
 # generates a list of all the permutations of a given string
 def perm_gen_lex(a):
-    final_perm_list = []
-    if len(a) == 0:
+    final_perm_list = []        # list to put all permutations in
+    if len(a) == 0:             # base case
         final_perm_list = []
-    elif len(a) == 1:
+    elif len(a) == 1:           # base case
         return [a]
     else:
         n = 0
-        while n < len(a):
-            new_a = a[:n] + a[n + 1:]
-            #print(new_a)
-            char = a[n]
-            perm_list = perm_gen_lex(new_a)
+        while n < len(a):                       # iterate through every letter
+            new_a = a[:n] + a[n + 1:]           # create new string without letter
+            char = a[n]                         # store letter that we're on
+            perm_list = perm_gen_lex(new_a)     # call recursive function with new string
             n += 1
             i = 0
             while i < len(perm_list):
-                perm_list[i] = char + perm_list[i]
+                perm_list[i] = char + perm_list[i]      # add character taken out to every partial permutation
                 i += 1
-            final_perm_list = final_perm_list + perm_list
+            final_perm_list = final_perm_list + perm_list   # combine permutation strings
     return final_perm_list
